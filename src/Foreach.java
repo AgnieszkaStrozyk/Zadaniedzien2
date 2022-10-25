a2f89d3bfe9a381a0a2c3fb38c3e7762c91d56df
Szkolenie <agnieszka.strozyk@gmail.com>
2022-10-25 20:17:24 +0200

    tablica kotów

---
+++ src/Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String [] catTable=new String[] {"Tery", "Laki", "Leo", "Zeo"};
+        for (String cat: catTable) {
+            System.out.println(cat);
+        }
+    }
+}
